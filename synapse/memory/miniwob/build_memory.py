d9f90842072e24f63776e5ece75d7af978782260
takira <ca.fie.0220@gmail.com>
2024-12-28 22:27:06 +0900

    memory/miniwobからcompwobの情報を削除 -> これでも動作OK

--- synapse/memory/miniwob/build_memory.py
+++ synapse/memory/miniwob/build_memory.py
@@ -61,18 +61,12 @@ EXEMPLAR_LIST = [
     "tic-tac-toe",
     "use-autocomplete",
     "use-slider",
-    "use-spinner",
-    "click-option_enter-text",                      # compwob
-    "click-option_enter-text-reverse",              # compwob
-    "click-option_login-user",                      # compwob
-    "click-option_login-user-reverse",              # compwob
-    "click-option_login-user-transition",           # compwob
-    "click-option_login-user-transition-reverse",   # compwob
-    "click-button-test"  # add for testing
+    "use-spinner"
 ]
 
 
 def build_memory(memory_path: str):
+    # print(f"memory_path: {memory_path}")
     with open(os.path.join(memory_path, "specifiers.json"), "r") as rf:
         specifier_dict = json.load(rf)
         exemplar_names = []
@@ -82,6 +76,10 @@ def build_memory(memory_path: str):
             for query in v:
                 exemplar_names.append(k)
                 specifiers.append(query)
+    # print(f"len(list(set(exemplar_names))): {len(list(set(exemplar_names)))}")
+    # print(f"len(EXEMPLAR_LIST): {len(EXEMPLAR_LIST)}")
+    # missing_elements = set(EXEMPLAR_LIST) - set(exemplar_names)
+    # print(f"missing: {missing_elements}")
     assert sorted(list(set(exemplar_names))) == sorted(EXEMPLAR_LIST)
 
     # embed memory_keys into VectorDB
@@ -98,11 +96,19 @@ def build_memory(memory_path: str):
 
 
 def retrieve_exemplar_name(memory, query: str, top_k) -> str:
+    """
+    FAISSからqueryに近い要素をtop_k取得し、最頻値のnameを返す
+    """
+    print(f"memory retrieve_exemplar_name()")
     retriever = memory.as_retriever(search_kwargs={"k": top_k})
+    print(f" retriever: {retriever}")
     docs = retriever.get_relevant_documents(query)
+    print(f" docs: {docs}")
     retrieved_exemplar_names = [doc.metadata["name"] for doc in docs]
+    print(f" retrieved_exemplar_names: {retrieved_exemplar_names}")
     logger.info(f"Retrieved exemplars: {retrieved_exemplar_names}")
     data = Counter(retrieved_exemplar_names)
+    print(f" data: {data}")
     retrieved_exemplar_name = data.most_common(1)[0][0]
 
     return retrieved_exemplar_name
